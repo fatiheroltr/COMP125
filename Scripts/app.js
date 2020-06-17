54e16553a00cbf7afaf526bc23ad504879da57c2
fatiheroltr <fatiheroltr@gmail.com>
2020-06-17 16:33:36 -0400

    Last changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,16 @@
 // IIFE - Immediately Invoked Function Expression = Anonymous Self-Executing Function - Closure
 (function(){
+
+
     
     function Start()
     {
-        console.log("MyApp Started...")
+        console.log('%cMyApp Started...',"font-size:20px;")
+    }
+
+    function Second()
+    {
+        console.log(mynum);
     }
 
     window.addEventListener("load", Start);
